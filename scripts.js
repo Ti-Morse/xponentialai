fd5169a3a0f145452e2cb31b51417c7a87eabd6c
Ti-Morse <56986978+Ti-Morse@users.noreply.github.com>
2023-08-16 06:41:47 -0800

    X

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
-  if (window.scrollY > 0) {
-    header.classList.add('transparent');
-  } else {
-    header.classList.remove('transparent');
-  }
-});
+
